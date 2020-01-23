45d316dc53c40d7fc1995800e6f5e3abced339c1
squarekzmlviy <square-kzmlviy@outlook.jp>
2020-01-24 01:08:28 +0900

    numpyに対応

--- 3-1step_func.py
+++ 3-1step_func.py
@@ -1,6 +1,14 @@
+import numpy as np
+
 def step_function(x):
     # 引数に実数しか対応できない(numpy配列を受け付けない)
-    if x > 0:
-        return 1
-    else:
-        return 0
+
+    #NumPy配列に対し不等号の演算を用いれば各要素にブーリアン型の生成される
+    y = x > 0
+    
+    #astype()メソッドで任意の型（今回はnp.int型）に変更できる
+    return y.astype(np.int)
+
+input_data = np.array([1.0, 2.0, 3, 0])
+output_data = step_function(input_data)
+print(output_data)
